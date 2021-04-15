5b4b04c7a382ddf493464d92ce906ee6385fe992
Pramod Kumar <Pramod@Pummy-cbz.home>
2021-04-15 03:53:33 -0400

    scope.js

--- JS_ES6/scope.js
+++ JS_ES6/scope.js
@@ -1,1 +1,18 @@
-console.log("Learning MERN Stack");
+// Scope
+
+var beatles = ["Paul","George","Pramod"];
+
+function printNames(names){
+    console.log("Inside of printNames()");
+    
+    function actuallyPrintingNames(){
+        for (var index=0; index< names.length; index++) {
+            var name = names[index];
+
+            console.log(name + ' was found at index ' + index);
+        }
+        console.log(' name and index after loop is ' + name + ' ; ' + index);
+    }
+    actuallyPrintingNames();
+}
+printNames(beatles);
